aab427c44faeccd1dda75f0aef8add271df47ebc
coderFeedForwardAlg <gaspersscottma@gmail.com>
2023-02-06 21:37:23 -0500

    tenserFLowTest

--- src/componants/mainPage.js
+++ src/componants/mainPage.js
@@ -5,7 +5,7 @@ import * as tf from '@tensorflow/tfjs';
 const MainPage = () => {
     const [pairs, setPairs] = useState([
         { x: -1, y: -3},
-        {x: 0, y:-1},
+        {x: 0, y:-1}, 
         {x: 1, y: 1},
         {x: 2, y: 3},
         {x: 5, y: 5},
